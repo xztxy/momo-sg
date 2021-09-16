da3c602d0fa754e16f743e2eea37c94fcce5dcf6
xztxy <31698398+xztxy@users.noreply.github.com>
2021-09-16 17:14:25 +0800

    Update dev.go

--- dev.go
+++ dev.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-     //青龙2.9机器人
-     _ "/root/momo-sg/develop/jd_asset/momo-sg/tree/main/develop/qinglong"
-     //京东资产查询
-     _ "/root/momo-sg/develop/jd_asset/tree/main/develop/jd_cookie"
-     //京东资产查询
-     _ "/root/momo-sg/develop/jd_asset/tree/main/develop/jd_help"
-     //京东资产查询
-     _ "/root/momo-sg/develop/jd_asset/tree/main/develop/jd_asset"
-) 
+  //青龙2.9机器人
+  _ "github.com/cdle/sillyGirl/develop/qinglong"
+  //京东资产查询
+  _ "github.com/xztxy/momo-sg/develop/jd_cookie"
+  //京东资产查询
+  _ "github.com/xztxy/momo-sg/develop/jd_help"
+  //京东资产查询
+  _ "github.com/xztxy/momo-sg/develop/jd_asset"
+  //腾讯qq
+  _"github.com/cdle/sillyGirl/im/qq"
+  //Telegram
+  _"github.com/cdle/sillyGirl/im/tg"
+)
