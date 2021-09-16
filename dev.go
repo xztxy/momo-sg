f9f68e15db446df85898518ce50f8b04767e10fb
xztxy <31698398+xztxy@users.noreply.github.com>
2021-09-16 17:25:31 +0800

    Update dev.go

--- dev.go
+++ dev.go
@@ -1,12 +1,12 @@
 package main
 
-import _ "./root/momo-sg/develop/qinglong/"
+import _ "develop/qinglong/"
   //京东资产查询
-import _ "./root/momo-sg/develop/jd_cookie"
+import _ "develop/jd_cookie"
   //京东资产查询
-import _ "./root/momo-sg/develop/jd_help"
+import _ "develop/jd_help"
   //京东资产查询
-import _ "./root/momo-sg/develop/jd_asset"
+import _ "develop/jd_asset"
   //腾讯qq
 import _ "github.com/cdle/sillyGirl/im/qq"
   //Telegram
